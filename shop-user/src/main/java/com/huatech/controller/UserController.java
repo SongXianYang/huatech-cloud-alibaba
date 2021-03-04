0b0d11fc44d0949578ec6ec4c0d5185b30c36c87
SongXianYang <1965569785@qq.com>
2021-03-04 11:40:55 +0800

    nacos -config

--- shop-user/src/main/java/com/huatech/controller/UserController.java
+++ shop-user/src/main/java/com/huatech/controller/UserController.java
@@ -2,6 +2,8 @@ package com.huatech.controller;
 
 import com.huatech.entity.User;
 import com.huatech.service.IUserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,13 +18,28 @@ import java.util.List;
  * @create: 2021-02-27 16:43
  **/
 @RestController
+@RefreshScope//只需要在需要动态读取配置的类上添加此注解就可以
 @RequestMapping("user")
 public class UserController {
     @Resource
     IUserService userService;
 
-    @RequestMapping(value = "finds",method = RequestMethod.GET)
+    @Value("${config.name}")
+    private String name;
+
+    @Value("${config.env}")
+    private String env;
+
+    @RequestMapping(value = "finds", method = RequestMethod.GET)
     public List<User> finds() {
         return userService.finds();
+    }
+    @GetMapping("name")
+    public String getName() {
+        return name;
+    }
+    @GetMapping("evn")
+    public String getEvn() {
+        return env;
     }
 }
